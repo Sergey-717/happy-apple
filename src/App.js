a883f16f9e07e557d7c19e80eaef75ae705d4f88
Your Name <you@example.com>
2023-07-13 20:04:12 +0300

    opachki

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import Posts from "./components/Posts";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/happy-apple"
-          element={
-            <div className="App">
-              <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                  Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Learn React
-                </a>
-              </header>
-            </div>
-          }
-        ></Route>
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/happy-apple" element={<Posts />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
